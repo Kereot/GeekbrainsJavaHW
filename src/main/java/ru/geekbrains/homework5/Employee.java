5dba6e5fba1e1b5488d32ef1ec84ec67c375e963
Kereot <eepdmitriy@gmail.com>
2021-10-10 16:43:25 +0300

    Homework for Java.Level_1.Lesson_4 and 1 'fori' version of Java.Level_1.Lesson_3.Task_4.

---
+++ src/main/java/ru/geekbrains/homework5/Employee.java
@@ -0,0 +1,33 @@
+package ru.geekbrains.homework5;
+
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private double salary;
+    private int age;
+
+    public Employee(String name, String position, String email, String phone, double salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void showEmployeeInfo() {
+        System.out.println(
+                "Информация о сотруднике:\nФИО: " + this.name
+                + "\nДолжность: " + this.position
+                + "\nE-mail: " + this.email
+                + "\nНомер телефона: " + this.phone
+                + "\nЗарплата: " + this.salary
+                + "\nВозраст: " + this.age);
+    }
+}
---
+++ src/main/java/ru/geekbrains/homework5/MainApp.java
@@ -0,0 +1,60 @@
+package ru.geekbrains.homework5;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        Employee[] staffArr = new Employee[5];
+
+        staffArr[0] = new Employee(
+                "Виктор Кант",
+                "Редактор",
+                "kant_v@geekbrains.com",
+                "+1 123 98 658",
+                69750.25,
+                48
+        );
+        staffArr[1] = new Employee(
+                "Карл Габсбург",
+                "Директор",
+                "boss@geekbrains.com",
+                "+1 123 11 110",
+                260000.01,
+                53
+        );
+        staffArr[2] = new Employee(
+                "Петр Печкин",
+                "Секретарь",
+                "pechkin@geekbrains.com",
+                "+1 123 71 963",
+                45000.00,
+                22
+        );
+        staffArr[3] = new Employee(
+                "Лев Шульц",
+                "Бухгалтер",
+                "lion@geekbrains.com",
+                "+1 123 55 087",
+                75254.50,
+                39
+        );
+        staffArr[4] = new Employee(
+                "Игорь Крутой",
+                "Менеджер",
+                "igorthecool@geekbrains.com",
+                "+1 123 99 555",
+                45000.00,
+                27
+        );
+
+        for (int i = 0; i < staffArr.length; i++) {
+            if (staffArr[i].getAge() > 40) {
+                staffArr[i].showEmployeeInfo();
+                System.out.println();
+            }
+        }
+    }
+
+
+
+}
